1d0b26020b433eaf6012e5ebd05b984c2bdf7ac9
xbryyanx <bryanmontoya838@gmail.com>
2024-12-09 01:30:05 -0600

    Prueba

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-    backend "azurerm" {
-      resource_group_name = "DefaultResourceGroup-EUS"
-      storage_account_name = "711bryanstorage"
-      container_name = "711-75182-tfstate"
-      key = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "DefaultResourceGroup-EUS"
+    storage_account_name = "711bryanstorage"
+    container_name       = "711-75182-tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
